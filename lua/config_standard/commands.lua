80e40c0752e9976c9220661a9c4b056080260ad8
Sam Riesland <432659+samr@users.noreply.github.com>
2024-05-03 12:28:28 -0700

    Disable cpp block comment text object for now.

--- lua/config_standard/commands.lua
+++ lua/config_standard/commands.lua
@@ -12,25 +12,26 @@ vim.cmd "silent! command PackerSync lua require 'packer_plugins' require('packer
 vim.cmd "silent! command PackerUpdate lua require 'packer_plugins' require('packer').update()"
 vim.cmd "silent! command PackerClean lua require 'packer_plugins' require('packer').clean()"
 
+-- TODO: Enable this again, once we figure out the textobj error.
 -- Add a text object for selecting block C++ comments. Requires code in 'plugin/cpp_block_comment.vim'
 -- Note that this creates "ic" as a textobj for block comments that use //, while "ac" uses treesitter to create
 -- a textobj for block comments that use /**/.
-vim.api.nvim_exec([[
-call textobj#user#plugin('cppblockcomment', {
-    \   '-': {
-    \     '*sfile*': expand('<sfile>:p'),
-    \     'select-i-function': 'GetLinesForBlockComment',
-    \     'select-i': 'ic',
-    \   },
-    \ })
-
-augroup cpp_textobjs
-  autocmd!
-  autocmd FileType cpp call textobj#user#map('cppblockcomment', {
-  \   '-': {
-  \     'select-a': '<buffer> ac',
-  \     'select-i': '<buffer> ic',
-  \   },
-  \ })
-augroup END
-]], false)
+-- vim.api.nvim_exec([[
+-- call textobj#user#plugin('cppblockcomment', {
+--     \   '-': {
+--     \     '*sfile*': expand('<sfile>:p'),
+--     \     'select-i-function': 'GetLinesForBlockComment',
+--     \     'select-i': 'ic',
+--     \   },
+--     \ })
+-- 
+-- augroup cpp_textobjs
+--   autocmd!
+--   autocmd FileType cpp call textobj#user#map('cppblockcomment', {
+--   \   '-': {
+--   \     'select-a': '<buffer> ac',
+--   \     'select-i': '<buffer> ic',
+--   \   },
+--   \ })
+-- augroup END
+-- ]], false)
